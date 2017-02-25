ef310d4de312eb7889f305ddf1344b3b3477e2bf
robert0714 <robert0714@gmaill.com>
2017-02-26 06:52:32 +0800

    adjust for updating docker 1.13

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-echo "Installing Ansible..."
+echo "Installing Ansible  on CentOS..." 
 
-su -c 'rpm -Uvh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm'
-yum -y install ansible
-yum -y install jq
+A=`yum info ansible`
+
+testing=`[[ $A =~ 'Ansible' ]]`
+
+echo $testing
+
+echo  "############# ${testing} ##############"
+timedatectl set-timezone Asia/Taipei
+
+ 
+if [[ $testing != ""  ]]; then
+     echo "Ansible is already installed."
+else
+      echo "Ansible is not installed."
+      yum install -y wget
+      wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+      rpm -ivh  epel-release-7-8.noarch.rpm
+      rm -rf epel-release-7-8.noarch.rpm
+      
+fi
+
+yum  install -y ansible
+
 
 cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
+
+yum update -y
