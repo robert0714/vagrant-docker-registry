29f5e79362ccac0416c5e65a3b538e69e7f8472f
robert0714 <robert0714@gmaill.com>
2017-02-26 07:07:54 +0800

    fixbug

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,8 +18,8 @@ else
       echo "Ansible is not installed."
       yum install -y wget
       wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
-      rpm -ivh  epel-release-7-8.noarch.rpm
-      rm -rf epel-release-7-8.noarch.rpm
+      rpm -ivh  epel-release-7-9.noarch.rpm
+      rm -rf epel-release-7-9.noarch.rpm
       
 fi
 
